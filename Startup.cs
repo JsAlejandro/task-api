7d6a9fbda479404a08b8ae41d2dadc11cf63c4a9
Jesus Flores <jesus.flores@ivelopment.com>
2020-07-04 14:43:52 -0300

    add AutoMapper

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using taskmanager_api.Models;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 namespace taskmanager_api
 {
     public class Startup
@@ -26,6 +27,17 @@ namespace taskmanager_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(configuration =>{
+                configuration.CreateMap<UsersCreatedDTO, Users>();
+                configuration.CreateMap<UsersUpdatedDTO, Users>();
+                configuration.CreateMap<Users, UsersShowDTO>();
+                configuration.CreateMap<CommentsCreatedDTO, Comments>();
+                configuration.CreateMap<Comments, CommentsShowDTO>();
+                configuration.CreateMap<CommentsUpdatedDTO, Comments>();
+                configuration.CreateMap<Assignment, AssignmentShowDTO>();
+                configuration.CreateMap<AssignmentUpdatedDTO, Assignment>();
+                configuration.CreateMap<AssignmentCreatedDTO, Assignment>();
+            } ,typeof(Startup));
             services.AddDbContext<TaskdbContext> (options => options.UseMySql (Configuration.GetConnectionString ("DefaultConnection")));
             services.AddControllers ().AddNewtonsoftJson (options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddControllers();
