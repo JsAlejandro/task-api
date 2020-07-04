c36c2c5204874e5c858c37df18506f93f5bfca0d
Jesus Flores <jesus.flores@ivelopment.com>
2020-07-04 14:29:46 -0300

    add model show DTO

---
+++ Models/Show/AssignmentShowDTO.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using taskmanager_api.Attribute;
+namespace taskmanager_api.Models {
+    public partial class AssignmentShowDTO {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength (20)]
+        public string Title { get; set; }
+
+        [Required]
+        [StatusTask]
+        public string Status { get; set; }
+
+        [Required]
+        [MaxLength (500)]
+        public string Description { get; set; }
+        public bool SoftDeleted { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Models/Show/CommentsShowDTO.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace taskmanager_api.Models {
+    public partial class CommentsShowDTO {
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength (500)]
+        public string Messages { get; set; }
+
+        public bool SoftDeleted { get; set; }
+
+        [Required]
+        public int AssignmentId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
