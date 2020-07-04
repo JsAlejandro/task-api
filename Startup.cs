33bab4a028e5b267c61f148549365c6536f0ace2
Jesus Flores <jesus.flores@ivelopment.com>
2020-07-04 14:43:07 -0300

    add AddNewtonsoftJson

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,8 @@ namespace taskmanager_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-             services.AddDbContext<TaskdbContext> (options => options.UseMySql (Configuration.GetConnectionString ("DefaultConnection")));
+            services.AddDbContext<TaskdbContext> (options => options.UseMySql (Configuration.GetConnectionString ("DefaultConnection")));
+            services.AddControllers ().AddNewtonsoftJson (options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddControllers();
         }
 
