93c6bf5b38160f20c4a0760138670f47836717f8
Jesus Flores <jesus.flores@ivelopment.com>
2020-07-17 22:12:25 -0300

    add populete

---
+++ Models/Populate/UsersPopulateDTO.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using taskmanager_api.Attribute;
+namespace taskmanager_api.Models {
+    public partial class UsersPopulateDTO {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        [MaxLength (15)]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength (5)]
+        [RoleUser]
+        public string Role { get; set; }
+        public bool SoftDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
