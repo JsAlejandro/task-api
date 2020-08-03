a0fba998364b77968f4e64db29a7939cea3849f9
Jesus Flores <jesus.flores@ivelopment.com>
2020-08-03 19:43:15 -0300

    change scrip sql

--- _MySQL_Init_Script/init.sql
+++ _MySQL_Init_Script/init.sql
@@ -36,7 +36,7 @@ CREATE TABLE `Assignment` (
   PRIMARY KEY (`Id`),
   KEY `IX_Assignment_UserId` (`UserId`),
   CONSTRAINT `FK_Assignment_Users_UserId` FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `Assignment` (
 
 LOCK TABLES `Assignment` WRITE;
 /*!40000 ALTER TABLE `Assignment` DISABLE KEYS */;
-INSERT INTO `Assignment` VALUES (1,'2020-07-04 22:28:29.791259','2020-07-04 22:29:08.050723','Add Comment','accept','Esta es una tarea de pruebas',1,1);
+INSERT INTO `Assignment` VALUES (2,'2020-07-26 01:00:03.567559','2020-07-26 01:00:03.567559','Update patch','accept','Create Task',1,3);
 /*!40000 ALTER TABLE `Assignment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,7 +78,7 @@ CREATE TABLE `Comments` (
 
 LOCK TABLES `Comments` WRITE;
 /*!40000 ALTER TABLE `Comments` DISABLE KEYS */;
-INSERT INTO `Comments` VALUES (1,'2020-07-04 22:31:13.719281','2020-07-04 22:36:31.734508','Este es una tarea imposible de hacer',1,1,1),(2,'2020-07-04 22:32:16.268165','2020-07-04 22:33:46.470678','Este es una tarea imposible de hacer',1,1,1);
+INSERT INTO `Comments` VALUES (2,'2020-07-26 01:00:39.747772','2020-07-26 01:04:02.467016','Este es un update patch 2',1,2,3);
 /*!40000 ALTER TABLE `Comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,11 +96,12 @@ CREATE TABLE `Users` (
   `FirstName` varchar(15) CHARACTER SET utf8mb4 NOT NULL,
   `LastName` varchar(15) CHARACTER SET utf8mb4 DEFAULT NULL,
   `Email` longtext CHARACTER SET utf8mb4 NOT NULL,
-  `Password` varchar(15) CHARACTER SET utf8mb4 NOT NULL,
+  `Password` longtext CHARACTER SET utf8mb4 NOT NULL,
   `Role` varchar(5) CHARACTER SET utf8mb4 NOT NULL,
   `SoftDeleted` tinyint(1) NOT NULL,
+  `Salt` longblob,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,7 +110,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'2020-07-04 22:11:51.569368','2020-07-04 22:39:55.323910','Alejandro','Flores','jesus1992@gmail.com','jafs1235A*','owner',1),(2,'2020-07-04 22:13:43.290331','2020-07-04 22:13:43.290291','Alejandro','Flores','jesus1992@gmail.com','jafs1235A*','owner',0);
+INSERT INTO `Users` VALUES (2,'2020-07-04 22:13:43.290331','2020-07-04 22:13:43.290291','Jose','Flores','jesus1992@gmail.com','jafs1235A*','owner',0,NULL),(3,'2020-07-18 00:10:37.485596','2020-07-18 00:10:37.485596','Alejandro','Sanchez','enrrique@gmail.com','jafs1235A*','owner',1,NULL),(4,'2020-08-01 19:02:58.038667','2020-08-01 19:02:58.038630','Alejandro','Flores','jesus1992@.com','gxaJKswwJ5Q2fAKGOcj/JNpYvrYZDwQim3vbObn3a6E=','owner',0,_binary '�W�\�HV��c��Dz\��)');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,7 +134,7 @@ CREATE TABLE `__EFMigrationsHistory` (
 
 LOCK TABLES `__EFMigrationsHistory` WRITE;
 /*!40000 ALTER TABLE `__EFMigrationsHistory` DISABLE KEYS */;
-INSERT INTO `__EFMigrationsHistory` VALUES ('20200704220618_InitialCreate','3.1.4');
+INSERT INTO `__EFMigrationsHistory` VALUES ('20200704220618_InitialCreate','3.1.4'),('20200801184214_Salt','3.1.4'),('20200801190210_changeLongPassword','3.1.4');
 /*!40000 ALTER TABLE `__EFMigrationsHistory` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -146,4 +147,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-09 11:25:29
+-- Dump completed on 2020-08-03 19:37:18
