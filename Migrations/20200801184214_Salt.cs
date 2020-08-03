05838deb72866f08aece105c2e4c8b6e9ffdca5f
Jesus Flores <jesus.flores@ivelopment.com>
2020-08-03 19:39:14 -0300

    add salt

---
+++ Migrations/20200801184214_Salt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace taskmanager_api.Migrations
+{
+    public partial class Salt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Salt",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+        }
+    }
+}
---
+++ Migrations/20200801190210_changeLongPassword.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace taskmanager_api.Migrations
+{
+    public partial class changeLongPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15) CHARACTER SET utf8mb4",
+                oldMaxLength: 15);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "varchar(15) CHARACTER SET utf8mb4",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
