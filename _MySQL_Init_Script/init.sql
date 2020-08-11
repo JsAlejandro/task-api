13f7f42e596677717788bef3db3e587a22d6c50b
Jesus Flores <jesus.flores@ivelopment.com>
2020-08-10 22:18:03 -0300

    new script

--- _MySQL_Init_Script/init.sql
+++ _MySQL_Init_Script/init.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `taskdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `taskdb`;
 -- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: taskdb
@@ -33,10 +31,11 @@ CREATE TABLE `Assignment` (
   `Description` varchar(500) CHARACTER SET utf8mb4 NOT NULL,
   `SoftDeleted` tinyint(1) NOT NULL,
   `UserId` int(11) NOT NULL,
+  `AssigneeId` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`Id`),
   KEY `IX_Assignment_UserId` (`UserId`),
   CONSTRAINT `FK_Assignment_Users_UserId` FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +44,7 @@ CREATE TABLE `Assignment` (
 
 LOCK TABLES `Assignment` WRITE;
 /*!40000 ALTER TABLE `Assignment` DISABLE KEYS */;
-INSERT INTO `Assignment` VALUES (2,'2020-07-26 01:00:03.567559','2020-07-26 01:00:03.567559','Update patch','accept','Create Task',1,3);
+INSERT INTO `Assignment` VALUES (2,'2020-07-26 01:00:03.567559','2020-08-11 01:00:08.914528','Add Comment','accept','Esta es una tarea de pruebas',0,2,0),(4,'2020-08-11 00:46:11.034599','2020-08-11 00:46:11.034598','Add Comment','accept','Esta es una tarea de pruebas',0,2,0),(5,'2020-08-11 00:56:13.570340','2020-08-11 00:56:13.570303','Add Comment','accept','Esta es una tarea de pruebas',0,2,0),(6,'2020-08-11 00:57:05.631274','2020-08-11 00:57:05.631273','Add Comment','accept','Esta es una tarea de pruebas',0,2,2);
 /*!40000 ALTER TABLE `Assignment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +109,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (2,'2020-07-04 22:13:43.290331','2020-07-04 22:13:43.290291','Jose','Flores','jesus1992@gmail.com','jafs1235A*','owner',0,NULL),(3,'2020-07-18 00:10:37.485596','2020-07-18 00:10:37.485596','Alejandro','Sanchez','enrrique@gmail.com','jafs1235A*','owner',1,NULL),(4,'2020-08-01 19:02:58.038667','2020-08-01 19:02:58.038630','Alejandro','Flores','jesus1992@.com','gxaJKswwJ5Q2fAKGOcj/JNpYvrYZDwQim3vbObn3a6E=','owner',0,_binary '�W�\�HV��c��Dz\��)');
+INSERT INTO `Users` VALUES (2,'2020-07-04 22:13:43.290331','2020-07-04 22:13:43.290291','Jose','Flores','jesus1992@gmail.com','jafs1235A*','owner',0,NULL),(3,'2020-07-18 00:10:37.485596','2020-07-18 00:10:37.485596','Alejandro','Sanchez','enrrique@gmail.com','jafs1235A*','owner',1,NULL),(4,'2020-08-01 19:02:58.038667','2020-08-01 19:02:58.038630','Alejandro','Flores','jesus1992@.com','gxaJKswwJ5Q2fAKGOcj/JNpYvrYZDwQim3vbObn3a6E=','owner',0,_binary 'ŠW©\ãHVc¶Dz\åý)');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +133,7 @@ CREATE TABLE `__EFMigrationsHistory` (
 
 LOCK TABLES `__EFMigrationsHistory` WRITE;
 /*!40000 ALTER TABLE `__EFMigrationsHistory` DISABLE KEYS */;
-INSERT INTO `__EFMigrationsHistory` VALUES ('20200704220618_InitialCreate','3.1.4'),('20200801184214_Salt','3.1.4'),('20200801190210_changeLongPassword','3.1.4');
+INSERT INTO `__EFMigrationsHistory` VALUES ('20200704220618_InitialCreate','3.1.4'),('20200801184214_Salt','3.1.4'),('20200801190210_changeLongPassword','3.1.4'),('20200811004439_addAssigneeId','3.1.4');
 /*!40000 ALTER TABLE `__EFMigrationsHistory` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -147,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-03 19:37:18
+-- Dump completed on 2020-08-10 22:16:14
